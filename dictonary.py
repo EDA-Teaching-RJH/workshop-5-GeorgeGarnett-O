380430028efbadbe095cf20fbc82ffae73a31147
GeorgeGarnett-O <gg403@kent.ac.uk>
2024-10-24 12:28:42 +0000

    practise part one ws 5

--- dictonary.py
+++ dictonary.py
@@ -1,7 +1,7 @@
 my_dictonary = {"name":"Geo" , "Age" : 20}
 
-another_dictonary = dictonary ( city = "london" , country = "UK")
+another_dictonary = dict ( city = "london" , country = "UK")
 
-print (my_dictonary["name"])
+print (my_dictonary ["name"])
 
-print (my_dictonary.get["age"])
+print (my_dictonary.get("age"))
